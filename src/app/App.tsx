5524e263dd655b2813b1519f37842390847ac3dc
qt314wink <qt.314wink@gmail.com>
2026-06-25 06:48:53 -0400

    feat(app): wire App.tsx and main.tsx entry point

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,16 @@
-import Header from '../components/layout/Header'
-import Footer from '../components/layout/Footer'
-import Hero from '../components/sections/Hero'
-import DesignSystem from '../components/sections/DesignSystem'
-import About from '../components/sections/About'
-import SelectedWork from '../components/sections/SelectedWork'
-import Journal from '../components/sections/Journal'
-import TypographySpecimen from '../components/sections/TypographySpecimen'
-import CTA from '../components/sections/CTA'
+import { Header } from '../components/layout/Header'
+import { Footer } from '../components/layout/Footer'
+import { Hero } from '../components/sections/Hero'
+import { DesignSystem } from '../components/sections/DesignSystem'
+import { About } from '../components/sections/About'
+import { SelectedWork } from '../components/sections/SelectedWork'
+import { Journal } from '../components/sections/Journal'
+import { TypographySpecimen } from '../components/sections/TypographySpecimen'
+import { CTA } from '../components/sections/CTA'
 
-export default function App() {
+export function App() {
   return (
-    <div className="bg-ivory font-sans text-ink antialiased">
+    <div className="min-h-screen">
       <Header />
       <main>
         <Hero />
